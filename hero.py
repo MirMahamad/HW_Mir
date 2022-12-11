56797d9efc5d488cdbaec5c78deef0ece0e0490c
MirMahamad <the.aman1414@gmail.com>
2022-12-11 16:30:42 +0600

    HomeWork2

--- hero.py
+++ hero.py
@@ -26,19 +26,18 @@ print(a.__len__())
 
 class FlyHero(SuperHero):
     people = 'people'
-    def __init__(self, name, nickname, superpower, health_point, catchphrase, fly, damage):
+    def __init__(self, name, nickname, superpower, health_point, catchphrase, damage, fly = False):
         super().__init__(name, nickname, superpower, health_point, catchphrase)
         self.fly = fly
-        self.fly = False
         self.damage = damage
 
     def hp(self):
-        self.fly = True
         return (f'Здоровье нашего героя {self.health_points ** 2}')
     def __str__(self):
-        return 'fly in the True_phrase'
+        self.fly = True
+        return (f'fly in the {self.fly}_phrase')
 
-a = FlyHero('Clark Joseph Kent','SuperMan','speed',100, "Loving another is hard, but hating is easy", "in the True_phrase", 100)
+a = FlyHero('Clark Joseph Kent','SuperMan','speed',100, "Loving another is hard, but hating is easy", 100,)
 print(a)
 print(a.name_hero())
 print(a.hp())
@@ -49,37 +48,41 @@ print(a.__str__())
 
 class SpaceHero(SuperHero):
     people = 'people'
-    def __init__(self, name, nickname, superpower, health_point, catchphrase, fly, damage):
+    def __init__(self, name, nickname, superpower, health_point, catchphrase, damage, fly = False):
         super().__init__(name, nickname, superpower, health_point, catchphrase)
         self.fly = fly
-        self.fly = False
         self.damage = damage
 
     def hp(self):
         self.fly = True
         return (f'Здоровье нашего героя {self.nickname} {self.health_points ** 2}')
 
-b = SpaceHero('Peter Jason Quill', 'Star Lord', 'dexterity', 200, "Don`t choke on your ambitions", "in the True_phrase", damage= 1000)
+b = SpaceHero('Peter Jason Quill', 'Star Lord', 'dexterity', 200, "Don`t choke on your ambitions",1000 )
 print(b)
 print(b.name_hero())
 print(b.hp())
 print(b.__len__())
-print(b.damage)
+print(a.__str__())
 
 class Villiain(SpaceHero):
-    def __init__(self, name, nickname, superpower, health_point, catchphrase, fly, damage):
-        super().__init__(name, nickname, superpower, health_point, catchphrase, fly, damage)
+    def __init__(self, name, nickname, superpower, health_point, catchphrase, damage, fly = False):
+        super().__init__(name, nickname, superpower, health_point, catchphrase, damage)
+        self.fly = fly
     def gen_x(self):
         pass
+    def Brand_phrase(self):
+        self.fly = True
+        return (f'fly in the {self.fly}_phrase')
     def crit(self):
         return (f"Критический урон героя {self.nickname} {self.damage **2}")
 
-c = Villiain('Anakin Skywalker', 'Darth Vader', 'anger', 50, "Luke, I'm your father!", "in the True_phrase", 190)
+c = Villiain('Anakin Skywalker', 'Darth Vader', 'anger', 50, "Luke, I'm your father!", 190)
 print(c)
 print(c.name_hero())
 print(c.hp())
 print(c.__len__())
 print(c.__str__())
+print(c.Brand_phrase())
 print(c.crit())
 print(Villiain.crit(b))
 print(Villiain.crit(a))
